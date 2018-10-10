99ec530e38d0641f0d4c007097143f9f3dd3bc7f
Abdul <hrfssa@gmail.com>
2018-10-10 15:29:54 -0700

    added react-native-navigation

--- app/index.js
+++ app/index.js
@@ -1,7 +1,3 @@
-/** @format */
+const { start } = require('./src/app');
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+start();
